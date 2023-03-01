ec6fcee5f029c72dc46d687554f802b65f149865
Muhammad Zaidan <89756297+zaidanm16@users.noreply.github.com>
2023-03-01 20:40:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Required Dependencies
-nala update -y
-nala install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+apt update -y
+apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 
 # Docker Repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-nala update -y
+apt update -y
 
 # Install Docker
-nala install docker-ce docker-ce-cli containerd.io docker-compose -y
+apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 
 # Check Status and Version
 systemctl status docker
